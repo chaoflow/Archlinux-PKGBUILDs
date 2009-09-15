831643573a4fd1fa1581f5c4c02429fd4b7c3bb0
Florian 'chaoflow' Friesdorf <flo@chaoflow.net>
2009-09-15 23:48:51 +0200

    wrapup.sh: based on git archive

--- wrapup.sh
+++ wrapup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -x
 
-dir=$1
-files=$( git ls-files |grep $dir)
+test -z $1 && exit 1
 
-echo $files |xargs tar cfz $1.tar.gz 
+# get rid of trailing /
+dir=$( basename $1 )
 
+git archive --format=tar --prefix=${dir}/ HEAD:${dir}/ | gzip > ${dir}.tar.gz
