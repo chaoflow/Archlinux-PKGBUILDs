f79deedfb28fe2cd9902d5319aef625c47ba62eb
Florian 'chaoflow' Friesdorf <flo@chaoflow.net>
2009-09-15 23:55:11 +0200

    wrapup: sanity check for PKGBUILD

--- wrapup.sh
+++ wrapup.sh
@@ -4,5 +4,9 @@ test -z $1 && exit 1
 
 # get rid of trailing /
 dir=$( basename $1 )
+tarball=${dir}.tar.gz
 
-git archive --format=tar --prefix=${dir}/ HEAD:${dir}/ | gzip > ${dir}.tar.gz
+git archive --format=tar --prefix=${dir}/ HEAD:${dir}/ | gzip > ${tarball}
+
+# sanity check - there needs to be a PKGBUILD otherwise we exit 1
+tar tfz ${tarball} |grep -q PKGBUILD || exit 1
