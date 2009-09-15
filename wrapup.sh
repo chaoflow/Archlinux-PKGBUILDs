00b2ed1bbfe7de299bd6212ab3975e72a4289237
Florian 'chaoflow' Friesdorf <flo@chaoflow.net>
2009-09-15 23:58:45 +0200

    wrapup: warning if no PKGBUILD

--- wrapup.sh
+++ wrapup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -x
 
+nopkgbuild() {
+    echo "ATTENTION: Created tarball $1 contains no PKGBUILD" 1>&2
+    exit 1
+}
+
 test -z $1 && exit 1
 
 # get rid of trailing /
@@ -9,4 +14,4 @@ tarball=${dir}.tar.gz
 git archive --format=tar --prefix=${dir}/ HEAD:${dir}/ | gzip > ${tarball}
 
 # sanity check - there needs to be a PKGBUILD otherwise we exit 1
-tar tfz ${tarball} |grep -q PKGBUILD || exit 1
+tar tfz ${tarball} |grep -q PKGBUILD || nopkgbuild ./${tarball}
