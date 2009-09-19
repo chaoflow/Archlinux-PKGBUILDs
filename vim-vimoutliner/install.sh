cf7f46c93592d6a9108193eefa3257a0ba4e9a03
Florian 'chaoflow' Friesdorf <flo@chaoflow.net>
2009-09-19 22:08:05 +0200

    vim-vimoutliner: based on vim-tvo

---
+++ vim-vimoutliner/install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+pkgname=$1
+srcdir=$2
+pkgdir=$3
+
+docdir=${pkgdir}/usr/share/doc/$pkgname
+install -d ${docdir}/
+install -m644 \
+    ${srcdir}/{FAQ.txt,INSTALL,MANIFEST,README.otl,changes.txt,versions.txt} \
+    ${docdir}/
+
+bindir=${pkgdir}/usr/bin
+install -d ${bindir}/
+install -m755 ${srcdir}/{doc2otl,opml2otl,otl2*,otlParser.rb,pod2otl} \
+    ${bindir}/
+
+sharedir=${pkgdir}/usr/share/$pkgname
+install -d ${sharedir}/
+install -m644 ${srcdir}/easy.{gvimrc,vimrc} ${sharedir}/
+
+bitmapdir=${sharedir}/bitmaps
+install -d ${bitmapdir}/
+install -m644 ${srcdir}/bitmaps/TVO/* ${bitmapdir}/
+
+macrosdir=${sharedir}/macros
+install -d ${macrosdir}/
+install -m644 ${srcdir}/macros/* ${macrosdir}/
+
+vimdir=${pkgdir}/usr/share/vim/vimfiles
+for x in doc ftdetect ftplugin syntax; do
+    install -d ${vimdir}/${x}/
+    install -m644 ${srcdir}/${x}/* ${vimdir}/${x}/
+done
