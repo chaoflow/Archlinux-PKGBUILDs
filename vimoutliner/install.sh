da6368be50d7e68081f4132f7cae0609bd2d1462
Florian 'chaoflow' Friesdorf <flo@chaoflow.net>
2009-09-19 21:38:49 +0200

    vimoutliner: reworked install.sh

--- vimoutliner/install.sh
+++ vimoutliner/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 pkgname=$1
 srcdir=$2
@@ -6,38 +6,18 @@ pkgdir=$3
 
 docdir=${pkgdir}/usr/share/doc/vimoutliner
 install -d ${docdir}/
-for x in \
-    FAQ.txt \
-    INSTALL \
-    MANIFEST \
-    README.otl \
-    changes.txt \
-    versions.txt
-do
- install -D -m644 ${srcdir}/${x} ${docdir}/
-done
+install -m644 \
+    ${srcdir}/{FAQ.txt,INSTALL,MANIFEST,README.otl,changes.txt,versions.txt} \
+    ${docdir}/
 
 bindir=${pkgdir}/usr/bin
 install -d ${bindir}/
-for x in \
-    doc2otl \
-    opml2otl \
-    otl2html \
-    otl2htmlList \
-    otl2pod \
-    otl2rtf \
-    otl2swiki \
-    otl2trac \
-    otlParser.rb \
-    pod2otl
-do
- install -m755 ${srcdir}/${x} ${bindir}/
-done
+install -m755 ${srcdir}/{doc2otl,opml2otl,otl2*,otlParser.rb,pod2otl} \
+    ${bindir}/
 
 sharedir=${pkgdir}/usr/share/vimoutliner
 install -d ${sharedir}/
-install -m644 ${srcdir}/easy.gvimrc ${sharedir}/easy.gvimrc
-install -m644 ${srcdir}/easy.vimrc ${sharedir}/easy.vimrc
+install -m644 ${srcdir}/easy.{gvimrc,vimrc} ${sharedir}/
 
 bitmapdir=${sharedir}/bitmaps
 install -d ${bitmapdir}/
@@ -49,9 +29,9 @@ install -m644 ${srcdir}/macros/* ${macrosdir}/
 
 vimdir=${pkgdir}/usr/share/vim/vimfiles
 for x in doc ftdetect ftplugin syntax; do
-install -d ${vimdir}/${x}/
-install -m644 ${srcdir}/${x}/* ${vimdir}/${x}/
+    install -d ${vimdir}/${x}/
+    install -m644 ${srcdir}/${x}/* ${vimdir}/${x}/
 done
 
 licensedir=${pkgdir}/usr/share/licenses/${pkgname}
-install -D -m755 ${srcdir}/license.txt ${licensedir}/license.txt
+install -D -m644 ${srcdir}/license.txt ${licensedir}/license.txt
